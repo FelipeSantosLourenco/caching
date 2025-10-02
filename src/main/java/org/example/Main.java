37eddd7c9e04d06df5c6367d358b8659c9038e47
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-10-01 21:19:01 -0300

    feat: first test with JEDIS

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import redis.clients.jedis.Jedis;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            Jedis jedis = new Jedis("localhost", 6379);
+
+            String response = jedis.ping();
+            System.out.println(response);
+
+            jedis.set("chave1", "valor1");
+
+            System.out.println(jedis.get("chave1"));
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar com jedis: " + e.getMessage());
+        }
     }
 }
